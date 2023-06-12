45d2abe250798dd58de380e4c339e4789cbabb4d
Nitin Sharma <80587065+NitinS87@users.noreply.github.com>
2023-06-12 21:32:16 +0530

    Update Login.jsx YouTube video
    Added YouTube video frame

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,16 +15,13 @@ const Login = () => {
       </div>
       <div className="flex flex-col lg:flex-row justify-center items-center mb-5 mx-auto gap-10 lg:gap-16">
         <div className="order-last lg:order-first">
-          <video
-            controls
-            className="w-[90%] lg:w-[500px] h-full lg:h-80 rounded-lg aspect-video mx-auto"
-          >
-            <source
-              src="https://drive.google.com/uc?export=download&id=1IBgsXOEevtHvbLXMiX6MwPYGIabjdPnH"
-              type="video/mp4"
-            />
-            Your browser does not support the video tag.
-          </video>
+          <iframe
+            className="w-full lg:w-[500px] h-full lg:h-80 aspect-video"
+            src="https://www.youtube-nocookie.com/embed/4bdydBhA-KE?rel=0"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
         </div>
         <div className="border-red-400 border-2 h-40 hidden lg:block"></div>
         <LoginForm />
