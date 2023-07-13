af5631e3263e078cea46177c3832dfa83bb97f2f
Nitin Sharma <nitin.sharma@coffeee.io>
2023-07-13 23:15:54 +0530

    Replaced video with image and removed 12 ways

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import techniques from "../assets/images/techniques.png";
+import techniques from "../assets/images/SPI Landing Page Images.svg";
 import LoginForm from "./LoginForm";
 
 const Login = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-6 mb-28">
+    <div className="flex flex-col justify-center items-center gap-6 mb-16">
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-gsl-light-red text-center text-3xl lg:text-5xl p-5 text-stroke">
           Student Processing Inventory (SPI)
@@ -13,21 +13,26 @@ const Login = () => {
           Embrace and Accommodate Neurodiversity
         </span>
       </div>
-      <div className="flex flex-col lg:flex-row justify-center items-center mb-5 mx-auto gap-10 lg:gap-16">
+      <div className="flex flex-col lg:flex-row justify-center items-center mx-auto gap-10 lg:gap-16">
         <div className="order-last lg:order-first">
-          <iframe
+          {/* <iframe
             className="w-full lg:w-[500px] h-full lg:h-80 aspect-video"
             src="https://www.youtube-nocookie.com/embed/4bdydBhA-KE?rel=0"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
-          ></iframe>
+          ></iframe> */}
+          <img
+            src={techniques}
+            alt="Banner"
+            className="w-[90%] lg:w-[500px] h-full lg:h-80 aspect-video mx-auto"
+          />
         </div>
         <div className="border-red-400 border-2 h-40 hidden lg:block"></div>
         <LoginForm />
       </div>
       <div>
-        <img src={techniques} alt="Techniques" className="w-[75%] mx-auto" />
+        {/* <img src={techniques} alt="Techniques" className="w-[75%] mx-auto" /> */}
       </div>
     </div>
   );
